2542fb840077c1a25ed73c835b094602b1578a6d
Ilya Petrov <ilya.muromec+42cc@gmail.com>
2011-05-07 16:28:32 +0300

    ticket:1 drop view, use generic

--- splash/urls.py
+++ splash/urls.py
@@ -1,6 +1,16 @@
+from django.views.generic.list_detail import object_detail
 from django.conf.urls.defaults import patterns, url
+
+from models import Person
 
 urlpatterns = patterns(
         'dj42cc_test.splash.views',
-        url('^$', 'show_person'),
+        url('^$', object_detail,
+            {   "template_name": "show_person.html",
+                "queryset" : Person.objects,
+                "object_id" : 1,
+                "template_object_name" : "person",
+            }
+        )
+
 )
