3015fd7a3630905e893ca2388fae19e529806222
Ilya Petrov <ilya.muromec+42cc@gmail.com>
2011-05-07 18:26:00 +0300

    ticket:3 more complex test, check actual log data

--- dj42cc_test/logger/tests.py
+++ dj42cc_test/logger/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from models import HttpLogEntry
+from uuid import uuid4
 
 class LogEmptyTest(TestCase):
     def test_nologs(self):
         logs = HttpLogEntry.objects.all()
-        
+
         self.assertEqual(logs.count(), 0)
 
 class LogWriteTest(TestCase):
@@ -15,3 +16,29 @@ class LogWriteTest(TestCase):
             count = HttpLogEntry.objects.count()
 
             self.assertEqual(count, x)
+
+    def tearDown(self):
+        HttpLogEntry.objects.all().delete()
+
+class LogWriteTest(TestCase):
+    TRIES = 20
+    PATHS = [str(uuid4()) for x in range(TRIES)]
+    PATHS.append("/")
+
+    def test_path(self):
+        responses = [
+                self.client.get(path)
+                for path in self.PATHS
+        ]
+
+        logs = HttpLogEntry.objects.all()
+
+        self.assertEqual(logs.count(), len(responses))
+
+        for path, response, log in zip(self.PATHS, responses, logs):
+            self.assertEqual(path, log.data.get('path'))
+            self.assertEqual("GET", log.data.get("method"))
+            self.assertEqual(response.status_code, log.data.get("code"))
+
+    def tearDown(self):
+        HttpLogEntry.objects.all().delete()
