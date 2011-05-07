90d77b67130791559aac0783daeca02e7f7ea09a
Ilya Petrov <ilya.muromec+42cc@gmail.com>
2011-05-07 15:10:50 +0300

    ticket:1 add contact and othercontact models

--- splash/models.py
+++ splash/models.py
@@ -5,3 +5,21 @@ class Person(models.Model):
     last_name = models.CharField(max_length=50)
     birth_date = models.DateField()
     bio = models.TextField()
+
+    def __unicode__(self):
+        return u'%s %s' % (self.first_name, self.last_name)
+
+class Contact(models.Model):
+    TYP_CHOICES = (
+        ("skype", "Skype"),
+        ("jabber", "JID"),
+        ("email", "E-mail"),
+    )
+    typ =  models.CharField(max_length=20, choices=TYP_CHOICES)
+    value = models.CharField(max_length=50)
+    person = models.ForeignKey(Person)
+
+# FIXME: should this really live in separete model?
+class OtherContact(models.Model):
+    person = models.ForeignKey(Person)
+    value = models.TextField()
