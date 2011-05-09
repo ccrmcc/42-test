6c95a79313acb922a5994059b9d88ef8b3770a14
Ilya Petrov <ilya.muromec+42cc@gmail.com>
2011-05-09 17:23:14 +0300

    ticket:6 report errors

--- dj42cc_test/splash/static/js/editpage.js
+++ dj42cc_test/splash/static/js/editpage.js
@@ -1,7 +1,35 @@
-var form_success = function(form) {
+var form_success = function(form, data, status) {
     $(form).find('input').removeAttr("disabled");
     $(form).find('textarea').removeAttr("disabled");
+    $(form).find("ul.errorlist").remove();
+    $('input.error').removeClass('error');
 
+    if(status != 'success') {
+        alert("internal server eror");
+        return
+    }
+    if(data.status=='ok') {
+        alert("saved ok");
+        return;
+    }
+
+    for(key in data.fields) {
+        var field = $("#id_"+key);
+        field.addClass("error");
+
+        var errorlist = $("<ul></ul>");
+        errorlist.addClass("errorlist");
+        errorlist.insertBefore(field);
+
+        var errors = data.fields[key];
+
+        for(i=0;i<errors.length; i++) {
+            box = $("<li></li>");
+            box.text(errors[i]);
+
+            errorlist.append(box);
+        }
+    }
 };
 
 var form_beforesubmit = function(form) {
@@ -12,8 +40,11 @@ var form_beforesubmit = function(form) {
 var ajaxify_form = function (form) {
 
     var options = {
-        success: function() { form_success(form) },
-        beforeSubmit: function() {form_beforesubmit(form)}
+        success: function(data, status) {
+                     form_success(form, data, status)
+        },
+        beforeSubmit: function() {form_beforesubmit(form)},
+        url : "/contact_edit/ajax",
     };
 
     $(form).ajaxForm(options);
