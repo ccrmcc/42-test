5633aef8fac9b3addf64c5457d3d6a49d3d475f2
Ilya Petrov <ilya.muromec+42cc@gmail.com>
2011-05-09 15:32:09 +0300

    ticket:6 serve satic data

--- dj42cc_test/urls.py
+++ dj42cc_test/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,3 +22,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
