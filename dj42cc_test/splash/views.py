2857f1bb0adaa12d336d05772d4dbdf518bd0655
Ilya Petrov <ilya.muromec+42cc@gmail.com>
2011-05-18 20:42:11 +0300

    ticket:5 migrate to inlineformset

--- dj42cc_test/splash/views.py
+++ dj42cc_test/splash/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from forms import PersonContactsForm, PersonForm
+from forms import PersonForm, ContactFormSet, OtherContactForm
 from models import Person
 from django.contrib.auth.decorators import login_required
 
@@ -8,24 +8,31 @@ from django.contrib.auth.decorators import login_required
 def edit_index_data(request):
 
     person = Person.objects.all().get()
+    other = person.othercontact_set.all().get()
 
     if request.method == 'GET':
-        out_data = person.contacts
-        for field in PersonForm.base_fields.keys():
-            out_data[field] = getattr(person, field)
+        form = PersonForm(instance=person)
+        contact_formset = ContactFormSet(instance=person)
+        othercontact_form = OtherContactForm(instance=other)
 
-        form = PersonContactsForm(out_data)
-
-        kw = {"form": form}
+        kw = {
+                "form": form,
+                "contact" : contact_formset,
+                "other" : othercontact_form,
+        }
 
         return render(request, 'edit_person.html', kw)
 
-    form = PersonContactsForm(request.POST, instance=person)
+    form = PersonForm(request.POST, instance=person)
+    contact_formset = ContactFormSet(request.POST, instance=person)
+    othercontact_form = OtherContactForm(request.POST, instance=other)
 
-    if form.is_valid():
+    if form.is_valid() and contact_formset.is_valid() and \
+            othercontact_form.is_valid():
         form.save()
+        contact_formset.save()
+        othercontact_form.save()
 
-        person.contacts = form.cleaned_data  # save contacts
 
         return redirect('/')
 
