5b7ffe948355346afe719ec8cb144cb2a7eb72f6
Ilya Petrov <ilya.muromec+42cc@gmail.com>
2011-05-20 22:20:47 +0300

    ticket:5 fix pep8 after merge

--- dj42cc_test/core/tests.py
+++ dj42cc_test/core/tests.py
@@ -22,6 +22,7 @@ class SettingsPageTest(TestCase):
             value = fmt % (key, getattr(settings, key))
             self.assertContains(response, value)
 
+
 class IndexLinkTest(TestCase):
     LINK = 'href="/settings"'
 
